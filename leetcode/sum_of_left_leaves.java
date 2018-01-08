95aed9917d4ade63c8423cc17e8b9ccae41ab051
kangtaku <jackkdw@gmail.com>
2018-01-09 01:29:21 +0900

    solved sum of left leaves

---
+++ leetcode/sum_of_left_leaves.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null)return 0;
+        int res=0;
+        if(root.left!=null&&root.left.left==null&&root.left.right==null)res=root.left.val;
+        return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right)+res;
+    }
+}
